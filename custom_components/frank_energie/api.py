e719c7d32853396a7d871d408ced32a4e42866dd
Hammie <sam@hammie.be>
2024-11-06 19:46:42 +0100

    Override FrankEnergie.DATA_URL class attribute

--- custom_components/frank_energie/api.py
+++ custom_components/frank_energie/api.py
@@ -1,6 +1,18 @@
 from python_frank_energie import FrankEnergie
 
-from .const import DATA_URL
+import logging
+from aiohttp.client import ClientSession
+from .const import DATA_URL as CONFIG_DATA_URL
+
+LOGGER = logging.getLogger(__name__)
 
 class FrankEnergieApi(FrankEnergie):
-    DATA_URL = DATA_URL
+    def __init__(
+            self,
+            clientsession: ClientSession = None,
+            auth_token: str | None = None,
+            refresh_token: str | None = None,
+    ):
+        FrankEnergie.DATA_URL = CONFIG_DATA_URL
+        super().__init__(clientsession, auth_token, refresh_token)
+        LOGGER.debug(f"Using {FrankEnergie.DATA_URL} for queries")
